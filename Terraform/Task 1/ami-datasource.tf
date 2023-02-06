b6b138c465b9745119ba7da31ad89ce37df85131
Julius <jzabulenas@protonmail.com>
2023-02-06 12:26:47 +0200

    Change Linux distro to Ubuntu 22.04, update httpd-install.sh file to work with Ubuntu

---
+++ Terraform/Task 1/ami-datasource.tf
@@ -0,0 +1,26 @@
+# Get latest AMI ID for Amazon Linux2 OS
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
+
+data "aws_ami" "amzlinux2" {
+  most_recent = true       #most recent AMI if there are multiple ami available
+  owners      = ["amazon"] #amazon owners
+  filter {
+    name   = "name"
+    #Not sure why, but it seems I need to explicitly state the version I want to use. I thought
+    #it should pull the most recent one based on line 6
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
